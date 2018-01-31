08b653fca79eb57b025b3d9e0975188797624300
Pramod <pramodnanduri@gmail.com>
2018-01-30 23:45:49 -0800

    Input handling

--- LinkedInScraper/js/scrape.js
+++ LinkedInScraper/js/scrape.js
@@ -2,42 +2,69 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true , })
 
-var waitforunload = function() {
-    return new Promise(function(resolve, reject) {
-        window.onbeforeunload = function() {
-            resolve();
-        };
-    });
-};
+var config = {
+    "username" : "dummy",
+    "password" : "dummy",
+    "linkedin" : "https://www.linkedin.com/",
+    "searchUrl" : "https://www.linkedin.com/search/results/people/?keywords=",
+    "query" : "dummy",
+    "search" : ""
+}
+
+var selectors = {
+    "email" : "input#login-email",
+    "password" : "input#login-password",
+    "submit" : "input#login-submit",
+    "profileClass" : ".search-result search-result__occluded-item"
+}
 
 
-nightmare
-	.goto('https://www.linkedin.com/')
-  .type('input#login-email', 'pramodnanduri@gmail.com')
-	.type('input#login-password', 'Mani2019')
-  .click('input#login-submit')
-	.wait('#nav-typeahead-wormhole')
-	.goto('https://www.linkedin.com/search/results/people/?keywords=Artificial%20Intelligence&origin=SWITCH_SEARCH_VERTICAL&page=2')
-	.wait('.search-result__info')
-	.evaluate(function() {
+handleInput();
+navigateHome();
 
-		var element = $('div.search-result__info a:first').attr('href');
-		return element;
-		//console.log("Element: " + element);
-	})
-  //.end()
-	//.goto('http://linkedin.com')
-    .then(function (result) {
-      console.log("Link : http://linkedin.com" + result)
-			profile = 'http://linkedin.com' + result;
-      extract(nightmare, profile);
-    })
 
-    .catch(function (error) {
-      console.error('Error:', error);
-    });
+function handleInput() {
+  if (process.argv.length <= 4) {
+      console.log("Username, password, search term mandatory in same order!");
+  } else {
+    config.username = process.argv[2];
+    config.password = process.argv[3];
+    config.query = process.argv[4];
+    config.search = config.searchUrl + config.query;
+    console.log(config);
+  }
+}
+
+function navigateHome() {
+  nightmare
+  	.goto(config.linkedin)
+    .type(selectors.email, config.username)
+  	.type(selectors.password, config.password)
+    .click(selectors.submit)
+  	.wait('#nav-typeahead-wormhole')
+  	.goto(config.search)
+  	.wait('.search-result__info')
+  	.evaluate(function() {
+  		var element = $('div.search-result__info a:first').attr('href');
+  		return element;
+  		//console.log("Element: " + element);
+  	})
+    //.end()
+  	//.goto('http://linkedin.com')
+      .then(function (result) {
+        console.log("Link : http://linkedin.com" + result)
+  			profile = 'http://linkedin.com' + result;
+        extract(nightmare, profile);
+      })
+
+      .catch(function (error) {
+        console.error('Error:', error);
+      });
+}
 
 function extract(nightmare, profile) {
+
+
   console.log("Extracting: " + profile);
   nightmare.goto(profile)
   .wait("h1.pv-top-card-section__name")
@@ -47,11 +74,11 @@ function extract(nightmare, profile) {
     var data = {
       "name" : name,
       "company" : company.trim(),
+      //"url" : profile
     };
 		return data;
 		//console.log("Element: " + element);
 	}).then(function(result){
       console.log(result);
   })
-
 }
